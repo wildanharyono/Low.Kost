c89370922523b686f72bdb4bcf6a145a5ae6d5d4
novalcahyoo <novalcahyo33@gmail.com>
2018-02-26 10:39:27 +0700

    commit pertama

---
+++ app/src/main/java/com/example/haryono/lowkost/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.haryono.lowkost;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT=4000; //Inisiasi splash
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toast.makeText(this,"KELOMPOK 8",Toast.LENGTH_LONG).show();
+        new Handler().postDelayed(new Runnable() { //inisiasi handler
+            @Override
+            public void run() { //inisiasi method run
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class); //inisiasi intent untuk pindah ke activity lain
+                startActivity(homeIntent);  //memanggil method start
+                finish(); //memanggil method finish
+            }
+        },SPLASH_TIME_OUT); //tag penutup splash
+    }
+}
